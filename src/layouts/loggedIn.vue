f2ad68ac6334496341179db5a38556af57801e0a
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2022-02-25 17:46:32 +0100

    feat(): change main page

--- src/layouts/loggedIn.vue
+++ src/layouts/loggedIn.vue
@@ -1,14 +1,10 @@
 <template>
   <TheNavbar />
-  <div class="container">
-    <TheTabs />
-    <slot />
-  </div>
+  <slot />
 </template>
 
 <script setup lang="ts">
 import TheNavbar from '@/components/TheNavbar.vue'
-import TheTabs from '@/components/TheTabs.vue'
 
 </script>
 
---
+++ src/views/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <TheTabs />
+  </div>
+</template>
+
+<script setup lang="ts">
+import TheTabs from '@/components/TheTabs.vue'
+</script>
+
+<style lang="scss" scoped>
+
+</style>
