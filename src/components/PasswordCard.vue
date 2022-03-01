e008b57131ca0d4e985da304f54801d2a853e010
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2022-03-01 16:41:07 +0100

    feat(): added dynamic background color to password cards and added modal to display details

--- src/components/PasswordCard.vue
+++ src/components/PasswordCard.vue
@@ -1,20 +1,32 @@
 <template>
-  <n-card :title="title" size="small">
-      Card Content
-  </n-card>
+  <div id="password-card">
+    <n-card
+      :title="title"
+      :style="{ backgroundColor: color }"
+      size="small"
+      class="pass-card"
+    >Card Content</n-card>
+  </div>
 </template>
 
 <script setup lang="ts">
-  import { defineProps } from 'vue'
+import { defineComponent, defineProps } from 'vue'
 
-  defineProps({
-    title: {
-      type: String,
-      required: true
-    }
-  })
+defineComponent({
+  name: 'passwordCard'
+})
+
+defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  color: {
+    type: String,
+    required: true
+  }
+})
 </script>
 
 <style scoped>
-
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,52 @@
 <template>
-  <div class="home">
+  <div id="home">
+    <n-modal
+      v-model:show="showPassword"
+      preset="card"
+      :title="title"
+      :style="bodyStyle"
+      :bordered="false"
+      size="huge"
+      :segmented="segmented"
+    >
+      <template #header-extra></template>
+      Content
+      <!-- <template #footer>Footer</template> -->
+    </n-modal>
     <n-grid cols="2 s:3 m:4 l:5 xl:6 2xl:7" responsive="screen">
       <n-grid-item class="m-1 p-1" v-for="(item, index) in items" :key="index">
-        <PasswordCard :title="item" />
+        <PasswordCard :title="item" @click="showModal(index)" :color="randomColor()"/>
       </n-grid-item>
     </n-grid>
   </div>
 </template>
 
 <script setup lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, Ref, ref } from "vue";
 import PasswordCard from "@/components/PasswordCard.vue";
 
+let showPassword: Ref<boolean> = ref(false);
+let title: Ref<string> = ref('')
 const items: string[] = ['Facebook', 'Twitter']
+const colors: string[] = ['#686868', '#707070', '#787878', '#808080']
+
+const segmented = {
+  content: 'soft',
+} as const
+
+const bodyStyle = {
+  width: '600px'
+}
+
+const randomColor = () => colors[Math.floor(Math.random() * colors.length)]
+
+const showModal = (index: number) => {
+  showPassword.value = !showPassword.value
+  title.value = items[index]
+}
 
 defineComponent({
   name: "Home"
 })
+
 </script>
