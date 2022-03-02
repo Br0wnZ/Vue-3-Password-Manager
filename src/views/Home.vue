922348a584ff3d7555b5c9ebec380bdba754f505
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2022-03-02 13:22:44 +0100

    feat(): applied loader and animations to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,9 +11,9 @@
     >
       <template #header-extra></template>
       Content
-      <!-- <template #footer>Footer</template> -->
     </n-modal>
-    <n-grid cols="2 s:3 m:4 l:5 xl:2 2xl:7" responsive="screen">
+    <Loading v-if="isLoading" />
+    <n-grid cols="2 s:3 m:4 l:5 xl:2 2xl:7" responsive="screen" class="bounce-in">
       <n-grid-item class="m-1 p-1" v-for="(password, index) in passwords" :key="index">
         <PasswordCard :password="password" @click="showModal(index)" :color="randomColor()" />
       </n-grid-item>
@@ -24,6 +24,7 @@
 <script setup lang="ts">
 import { defineComponent, onMounted, Ref, ref } from "vue";
 import PasswordCard from "@/components/PasswordCard.vue";
+import Loading from "@/components/Loading.vue";
 import { IPassword } from "@/models/password";
 import { getPasswords } from '@/services/passwordService'
 
@@ -33,7 +34,8 @@ defineComponent({
 
 let showPassword: Ref<boolean> = ref(false);
 let title: Ref<string> = ref('')
-let passwords: Ref<IPassword[]> = ref<IPassword[]>([]) 
+let passwords: Ref<IPassword[]> = ref<IPassword[]>([])
+let isLoading: Ref<boolean> = ref<boolean>(false)
 
 const colors: string[] = ['#686868', '#707070', '#787878', '#808080']
 
@@ -46,7 +48,9 @@ const bodyStyle = {
 }
 
 onMounted(async () => {
+  isLoading.value = true
   passwords.value = await getPasswords()
+  isLoading.value = false
 })
 
 const randomColor = () => colors[Math.floor(Math.random() * colors.length)]
@@ -57,3 +61,21 @@ const showModal = (index: number) => {
 }
 
 </script>
+
+<style scoped lang="scss">
+  .bounce-in {
+  animation: bounce-in 2s ease;
+}
+@keyframes bounce-in {
+  0% {
+    opacity: 0;
+    transform: scale(.3);
+  }
+  50% {
+    opacity: 1;
+    transform: scale(1.05);
+  }
+  70% { transform: scale(.9); }
+  100% { transform: scale(1); }
+}
+</style>
