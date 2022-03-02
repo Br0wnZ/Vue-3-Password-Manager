b1357d0ef5687d9324227395543dc1f215334d4c
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2022-03-02 12:16:46 +0100

    feat(): password card item style changed

--- src/components/PasswordCard.vue
+++ src/components/PasswordCard.vue
@@ -1,24 +1,27 @@
 <template>
   <div id="password-card">
-    <n-card
-      :title="title"
-      :style="{ backgroundColor: color }"
-      size="small"
-      class="pass-card"
-    >Card Content</n-card>
+    <n-card :style="style" size="medium" class="pass-card">
+      <div class="parent">
+        <img :src="password.icon" loading="lazy" />
+        <div class="flex-container">
+          <div class="flex-items">{{ password.name }}</div>
+        </div>
+      </div>
+    </n-card>
   </div>
 </template>
 
 <script setup lang="ts">
-import { defineComponent, defineProps } from 'vue'
+import { defineComponent, defineProps, PropType } from 'vue'
+import { IPassword } from "@/models/password"
 
 defineComponent({
   name: 'passwordCard'
 })
 
-defineProps({
-  title: {
-    type: String,
+const props = defineProps({
+  password: {
+    type: Object as PropType<IPassword>,
     required: true
   },
   color: {
@@ -26,7 +29,58 @@ defineProps({
     required: true
   }
 })
+
+const style = { backgroundColor: props.color }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+
+#password-card > .n-card {
+  background: rgba(255, 255, 255, 0.35);
+  box-shadow: 0 8px 32px 0 rgba(59, 60, 65, 0.37);
+  backdrop-filter: blur(20px);
+  -webkit-backdrop-filter: blur(20px) !important;
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
+
+}
+.parent {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-around;
+  align-items: normal;
+  align-content: space-between;
+
+  img {
+    border-radius: 50%;
+    height: 5vh;
+  }
+  .flex-container {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    justify-content: center;
+    align-items: normal;
+    align-content: space-between;
+  }
+
+  .flex-items:nth-child(1) {
+    display: block;
+    flex-grow: 0;
+    flex-shrink: 1;
+    flex-basis: auto;
+    align-self: auto;
+    order: 0;
+  }
+
+  .flex-items:nth-child(2) {
+    display: block;
+    flex-grow: 0;
+    flex-shrink: 1;
+    flex-basis: auto;
+    align-self: auto;
+    order: 0;
+  }
+}
 </style>
