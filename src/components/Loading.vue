e26715fdf4b5301bbc265264175a5143f2f6ac26
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2022-03-02 13:22:07 +0100

    feat(): loading component added

---
+++ src/components/Loading.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="center">
+    <n-space vertical>
+      <n-spin>
+        <template #description>
+          <n-gradient-text :gradient="gradientText">Loading...</n-gradient-text>
+        </template>
+      </n-spin>
+    </n-space>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface IGradient {
+  from: string
+  to: string
+}
+
+const gradientText: IGradient = {
+  from: 'rgb(85, 85, 85)',
+  to: 'rgb(170, 170, 170)'
+}
+</script>
+
+<style scoped>
+
+.center {
+  text-align: center;
+}
+</style>
